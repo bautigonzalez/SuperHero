6123dafc62d8596306a4f5a94a2a8a94fb42d085
bautigonzalez <batigonza@gmail.com>
2020-08-17 19:33:58 -0300

    carpeta front

---
+++ front/webpack.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      filename: "bundle.js",
+      path: __dirname + '/../back/public'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    context: __dirname,
+    module: {
+      rules: [
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+          'file-loader',
+          ],
+          },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+        {
+          test: /jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+          query: {
+            presets: [
+              "@babel/preset-react",
+              "@babel/env"
+            ]
+          }
+        }
+      ]
+    },
+    devtool: 'source-map'
+  }
