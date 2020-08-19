065149379f5b0a60bc1e74ffd21ba5e348460e13
bautigonzalez <batigonza@gmail.com>
2020-08-19 10:19:26 -0300

    card changes

--- front/src/components/card/Card.js
+++ front/src/components/card/Card.js
@@ -16,7 +16,10 @@ export default ({superhero}) => {
     return (
             <div className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 divSuperhero">
                 <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-                <img className="imgSuperhero" src={superhero.image.url} alt={superhero.name} onClick={handleClick}/>
+                <div className="divImg">
+                    <img className="imgSuperhero" src={superhero.image.url} alt={superhero.name} onClick={handleClick}/>
+                    <h4 className="cardText" onClick={handleClick}>{superhero.name}</h4>
+                </div>
                 <div className="cardSuperhero" onClick={handleClick}>
                     <h3>{superhero.name}</h3>
                 </div>
